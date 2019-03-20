aa612665d7994bd0681870ce1c1e4927f1af6fa2
dhmitry <dmitrijs.savkins@gmail.com>
2019-03-20 18:26:17 +0200

    initial

---
+++ src/main/JTimerFX.java
@@ -0,0 +1,121 @@
+package main;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.File;
+
+
+public class JTimerFX extends Application {
+    private static final int UPDATE_RATE = 50;
+
+    private Timeline timer;
+    private long remaining;
+
+    private Stage stage;
+    private Label label;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) {
+        this.stage = stage;
+        stage.setTitle("JTimerFX");
+        initialize();
+
+        stage.show();
+    }
+
+    private void initialize() {
+        timer = new Timeline();
+
+        label = new Label();
+        label.setFont(new Font(30));
+
+        reset();
+
+        Button start = new Button("START");
+        Button pause = new Button("PAUSE");
+        Button reset = new Button("RESET");
+
+        start.setOnAction(actionEvent -> start());
+        pause.setOnAction(actionEvent -> pause());
+        reset.setOnAction(actionEvent -> reset());
+
+        VBox.setVgrow(start, Priority.ALWAYS);
+        VBox.setVgrow(pause, Priority.ALWAYS);
+        VBox.setVgrow(reset, Priority.ALWAYS);
+        start.setMaxWidth(Double.MAX_VALUE);
+        pause.setMaxWidth(Double.MAX_VALUE);
+        reset.setMaxWidth(Double.MAX_VALUE);
+
+        HBox hbox = new HBox(5, label, new VBox(5, start, pause, reset));
+
+//        VBox layout = new VBox(5, hbox);
+//        layout.setPadding(new Insets(5));
+
+        Scene scene = new Scene(hbox);
+        stage.setScene(scene);
+    }
+
+    public void start() {
+        if (remaining > 0) {
+            pause();
+
+            timer.getKeyFrames().add(new KeyFrame(Duration.millis(UPDATE_RATE), actionEvent -> update()));
+            timer.setCycleCount(Animation.INDEFINITE);
+            timer.play();
+        }
+    }
+
+    public void pause() {
+        timer.stop();
+        timer.getKeyFrames().clear();
+    }
+
+    public void reset() {
+        pause();
+        remaining = 5000;
+        updateLabel();
+    }
+
+    private void update() {
+        if (remaining <= 0) {
+            reset();
+            done();
+        } else {
+            remaining -= UPDATE_RATE;
+            updateLabel();
+        }
+    }
+
+    private void updateLabel() {
+        label.setText(formatMilliseconds(remaining));
+    }
+
+    private String formatMilliseconds(long ms) {
+        int secs = (int) (ms / 1000 % 60);
+        int mins = (int) (ms / 1000 / 60);
+
+        return String.format("%02d:%02d.%03d", mins, secs, ms % 1000);
+    }
+
+    private void done() {
+        AudioClip clip = new AudioClip(new File("done.wav").toURI().toString());
+        clip.play();
+    }
+
+}
